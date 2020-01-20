7be09ce206f0fef2505f382348f81053c559dc4c
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-20 21:53:20 +0000

    add get request handling for /urls/new

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get('/urls.json', (req,res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls/new', (req,res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req,res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
