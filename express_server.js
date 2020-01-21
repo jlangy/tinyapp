0f6c0d2cd80d53b390ebd3b9ae3ad60e75c18371
Jonathan Langlois <jonathan-langlois@Live.ca>
2020-01-21 02:10:14 +0000

    Add post request server side, and link to update page from /urls

--- express_server.js
+++ express_server.js
@@ -60,6 +60,12 @@ app.post('/urls', (req,res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+app.post('/urls/:shortURL', (req,res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
 app.post('/urls/:shortURL/delete', (req,res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
